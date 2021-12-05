093c2147913ad54cc47eed4fe28229360f32a2ba
morez <m.sahelgozin@gmail.com>
2021-12-05 17:48:35 +0330

    add Access-Control-Allow-Origin header for responses

--- app/index.js
+++ app/index.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 // routes
 var routes = require('./../router/index');
 app.use('/', routes);
